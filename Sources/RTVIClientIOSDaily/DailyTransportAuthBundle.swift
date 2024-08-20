1070a065ce03c1408429aa9fe6384515e729fb54
Filipi Fuchter <filipi87@gmail.com>
2024-08-20 18:08:44 -0300

    Fixing bundle issue where it was expecting to receive roomName.

--- Sources/RTVIClientIOSDaily/DailyTransportAuthBundle.swift
+++ Sources/RTVIClientIOSDaily/DailyTransportAuthBundle.swift
@@ -1,15 +1,11 @@
 import Foundation
 
 struct DailyTransportAuthBundle: Codable {
-    let roomName: String
     let roomUrl: String
     let token: String?
-    let botConfig: String?
     
     enum CodingKeys: String, CodingKey {
-        case roomName = "room_name"
         case roomUrl = "room_url"
         case token
-        case botConfig = "bot_config"
     }
 }
