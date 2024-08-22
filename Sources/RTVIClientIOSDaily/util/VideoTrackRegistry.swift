57bf8d32920b61f2397149cfd3ba8519b2bf8522
Filipi Fuchter <filipi87@gmail.com>
2024-08-22 16:48:50 -0300

    Enabling the camera

--- Sources/RTVIClientIOSDaily/util/VideoTrackRegistry.swift
+++ Sources/RTVIClientIOSDaily/util/VideoTrackRegistry.swift
@@ -10,15 +10,15 @@ final class VideoTrackRegistry {
     static func registerTrack(originalTrack: VideoTrack, mediaTrackId: MediaTrackId) {
         trackMap[mediaTrackId] = originalTrack
     }
-    
+
     // Retrieves the original track
     static func getTrack(mediaTrackId: MediaTrackId) -> VideoTrack? {
-        trackMap[mediaTrackId]
+        return trackMap[mediaTrackId]
     }
 
     // Method to clear all tracks from the registry
     static func clearRegistry() {
         trackMap.removeAll()
     }
-    
+
 }
